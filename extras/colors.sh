8295f099a27f1681516920741160cb56564f7364
Joshua Frederick <josh@jfred.net>
2012-04-06 13:50:32 -0700

    Adding a script to print out the 256 term colors

---
+++ extras/colors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+COLUMNS=$(tput cols)
+( x=`tput op` y=`printf %$((${COLUMNS}-6))s`;for i in {0..256};do o=00$i;echo -e ${o:${#o}-3:3} `tput setaf $i;tput setab $i`${y// /=}$x;done; )
