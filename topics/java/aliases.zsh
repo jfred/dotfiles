85019408dff9b5fffe13f8c220da321b837883ee
Joshua Frederick <josh@jfred.net>
2012-10-09 13:45:26 -0600

    Updated quoted value for mpath result

--- topics/java/aliases.zsh
+++ topics/java/aliases.zsh
@@ -1,2 +1,2 @@
 # setup classpath from maven pom
-alias mpath='export CLASSPATH=`pwd`/src/main:`pwd`/src/test:`mvn dependency:build-classpath | grep jar`'
+alias mpath='export CLASSPATH="`pwd`/src/main:`pwd`/src/test:`mvn dependency:build-classpath | grep jar`"'
