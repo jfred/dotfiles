34e391653892a57218d0261e3477161e67f07b15
Joshua Frederick <josh@jfred.net>
2012-10-09 13:45:55 -0600

    Added some extra installs to the osx script

--- extras/osx.sh
+++ extras/osx.sh
@@ -2,9 +2,13 @@
 set -e
 
 brew update
+brew install coreutils
 brew install zsh
 brew install git
 brew install python
 brew install ruby
 brew install --HEAD macvim
 
+# tmux
+brew install tmux
+brew install reattach-to-user-namespace
