b547fd8a0bcc1d8714f02504165298fb599c0b38
Joshua Frederick <josh@jfred.net>
2013-12-23 19:14:54 -0800

    Adding shortcut for app installs from homebrew-cask

---
+++ extras/osx/app.sh
@@ -0,0 +1,9 @@
+#! /bin/bash -e
+
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
+brew cask install quicksilver
+brew cask install iterm2
+brew cask install mou
+brew cask install google-chrome
