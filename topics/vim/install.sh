476ddf43c08d5133660857b94e0bc404947f9277
Joshua Frederick <josh@jfred.net>
2015-12-26 12:59:11 -0800

    'Lock' vim-plug to specific commit

--- topics/vim/install.sh
+++ topics/vim/install.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
+# commit hash of vim-plug
+COMMIT_HASH=77413875dac6b4d147889d5a7c2e60a1929b5549
 BASEDIR=$(dirname $0)
 
-mkdir -p ~/.vim/autoload
-if [ ! -f ~/.vim/autoload/plug.vim ]; then
-    curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+LATEST_HASH=$(git ls-remote https://github.com/junegunn/vim-plug | grep master | awk '{ print $1}')
+
+if [ "${LATEST_HASH}" != "${COMMIT_HASH}" ]; then
+    echo "WARNING: vim-plug has been updated - latest commit: ${LATEST_HASH}"
 fi
+
+mkdir -p ~/.vim/autoload
+target=~/.vim/autoload/plug.vim
+tmpfile=$(mktemp /tmp/plug.vim.XXXXXX)
+curl -fLo ${tmpfile} https://raw.githubusercontent.com/junegunn/vim-plug/${COMMIT_HASH}/plug.vim > /dev/null 2>&1
+if ! cmp --silent ${tmpfile} ${target}; then
+    echo Setting plug.vim to ${COMMIT_HASH}
+    mv ${tmpfile} ${target}
+fi
+rm -f "$tmpfile"
 
 (cd ${BASEDIR} && vim -u vimrc_load +PlugClean! +PlugUpdate +UpdateRemotePlugins +qa!)
