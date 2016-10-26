723d4c19521408832901b0d129485d07d2dad177
Joshua Frederick <josh@jfred.net>
2016-10-26 09:00:46 -0700

    Some modified mjolnir bindings

--- topics/osx/mjolnir.symlink/init.lua
+++ topics/osx/mjolnir.symlink/init.lua
@@ -15,7 +15,21 @@ local function opendictionary()
   application.launchorfocus("Dictionary")
 end
 
+local function reloading()
+  alert.show("Reloading Mjolnir config", 0.75)
+  mjolnir.reload()
+end
+
+local function launch(app)
+  alert.show("Opening " .. app, 0.25)
+  application.launchorfocus(app)
+end
+
+hotkey.bind(mash, 'R', reloading)
 hotkey.bind(mash, 'D', opendictionary)
+hotkey.bind(mash, '`', function() launch("iTerm") end)
+hotkey.bind(mash, 'S', function() launch("Slack") end)
+hotkey.bind(mash, 'I', function() launch("IntelliJ IDEA") end)
 
 hotkey.bind(mash, ';', function() ext.grid.snap(window.focusedwindow()) end)
 hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), ext.grid.snap) end)
@@ -38,7 +52,12 @@ hotkey.bind(mash, 'K', ext.grid.pushwindow_up)
 hotkey.bind(mash, 'H', ext.grid.pushwindow_left)
 hotkey.bind(mash, 'L', ext.grid.pushwindow_right)
 
-hotkey.bind(mash, 'U', ext.grid.resizewindow_taller)
-hotkey.bind(mash, 'Y', ext.grid.resizewindow_shorter)
-hotkey.bind(mash, 'O', ext.grid.resizewindow_wider)
-hotkey.bind(mash, 'I', ext.grid.resizewindow_thinner)
+hotkey.bind(mash, '[', ext.grid.resizewindow_taller)
+hotkey.bind(mash, ']', ext.grid.resizewindow_shorter)
+hotkey.bind(mash, '.', ext.grid.resizewindow_wider)
+hotkey.bind(mash, ',', ext.grid.resizewindow_thinner)
+
+-- default positions
+hotkey.bind(mash, '1', function() ext.grid.adjust_focused_window(function(f) f.y = 0; f.x = 0; f.w = 1; f.h = 2; end) end)
+hotkey.bind(mash, '2', function() ext.grid.adjust_focused_window(function(f) f.y = 0; f.x = 0; f.w = 2; f.h = 2; end) end)
+hotkey.bind(mash, '3', function() ext.grid.adjust_focused_window(function(f) f.y = 0; f.x = 2; f.w = 1; f.h = 2; end) end)
