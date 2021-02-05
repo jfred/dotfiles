729dc53cd720668035e3c3c2db9e686c35577137
Joshua Frederick <josh@jfred.net>
2021-02-05 08:52:14 -0800

    system: remove g alias to git (never used)

--- topics/system/aliases.zsh
+++ topics/system/aliases.zsh
@@ -2,6 +2,5 @@ alias ls="ls -F --color=auto"
 alias l="ls -lAh"
 alias ll="ls -l"
 alias la='ls -A'
-alias g='git'
 
 alias ssh-add-all="find ~/.ssh -type f -name 'id_*' -not -name '*.pub' -execdir ssh-add {} \;"
