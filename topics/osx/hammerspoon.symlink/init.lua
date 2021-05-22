6d3bd459ab1b9d6e341a8d3cc2c2ea0607ef94d8
Joshua Frederick <josh@jfred.net>
2021-05-22 03:49:01 -0700

    osx: add <mash-N> to hammerspoon to move between monitors

--- topics/osx/hammerspoon.symlink/init.lua
+++ topics/osx/hammerspoon.symlink/init.lua
@@ -104,6 +104,17 @@ superbind('.', grid.resizeWindowWider)
 superbind(',', grid.resizeWindowThinner)
 superbind('M', grid.maximizeWindow)
 
+-- move windows
+superbind('N', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect 
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
+
 -- bind applications
 for key, name in pairs(apps) do
     superbind(key, launch(name))
