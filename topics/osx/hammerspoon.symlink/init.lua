045f3d85c1444bc832694c3b16986017e6566089
Joshua Frederick <josh@jfred.net>
2021-12-13 07:27:09 -0800

    osx: hammerpoon config updates. rearranging the app launcher

--- topics/osx/hammerspoon.symlink/init.lua
+++ topics/osx/hammerspoon.symlink/init.lua
@@ -6,76 +6,59 @@ local gridDims = {6, 4}
 local mega = {"cmd", "alt", "shift"}
 local hyper = {"cmd", "ctrl", "shift"}
 
-local apps = {
-    P='Slack',
-    U='iTerm',
-    O='Google Chrome',
-    I='IntelliJ IDEA',
-    Y='Pocket Casts'
-}
-
 local function reloading()
-    hs.alert("Reloading HS config...")
-    hs.reload()
+  hs.alert("Reloading HS config...")
+  hs.reload()
 end
 
 local function superbind(char, func)
-    hotkey.deleteAll(mega, char)
-    hotkey.bind(mega, char, func)
+  hotkey.deleteAll(mega, char)
+  hotkey.bind(mega, char, func)
 
-    hotkey.deleteAll(hyper, char)
-    hotkey.bind(hyper, char, func)
-end
-
-local function launch(app)
-    return function()
-        appFound = hs.application.find(app)
-        if appFound then
-            hs.application.launchOrFocusByBundleID(appFound:bundleID())
-        end
-    end
+  hotkey.deleteAll(hyper, char)
+  hotkey.bind(hyper, char, func)
 end
 
 -- default grid
 function printGrid()
-    local s = string.format('%sx%s', gridDims[1], gridDims[2])
-    hs.alert(string.format("grid: %s", s))
+  local s = string.format('%sx%s', gridDims[1], gridDims[2])
+  hs.alert(string.format("grid: %s", s))
 end
 function setGrid()
-    local s = string.format('%sx%s', gridDims[1], gridDims[2])
-    grid.setGrid(s)
+  local s = string.format('%sx%s', gridDims[1], gridDims[2])
+  grid.setGrid(s)
 end
 
 function increaseGridDim(index, max)
-    if gridDims[index] < max then
-        gridDims[index] = gridDims[index] + 1
-        setGrid()
-    end
-    printGrid()
+  if gridDims[index] < max then
+    gridDims[index] = gridDims[index] + 1
+    setGrid()
+  end
+  printGrid()
 end
 
 function decreaseGridDim(index)
-    if gridDims[index] > 1 then
-        gridDims[index] = gridDims[index] - 1
-        setGrid()
-    end
-    printGrid()
+  if gridDims[index] > 1 then
+    gridDims[index] = gridDims[index] - 1
+    setGrid()
+  end
+  printGrid()
 end
 
 function increaseGridWidth()
-    increaseGridDim(1, 6)
+  increaseGridDim(1, 6)
 end
 
 function decreaseGridWidth()
-    decreaseGridDim(1)
+  decreaseGridDim(1)
 end
 
 function increaseGridHeight()
-    increaseGridDim(2, 4)
+  increaseGridDim(2, 4)
 end
 
 function decreaseGridHeight()
-    decreaseGridDim(2)
+  decreaseGridDim(2)
 end
 
 -- reload config
@@ -116,8 +99,30 @@ superbind('N', function()
 end)
 
 -- bind applications
-for key, name in pairs(apps) do
-    superbind(key, launch(name))
+hs.application.enableSpotlightForNameSearches(true)
+
+local function launcher(app)
+  return function()
+    appFound = hs.application.find(app)
+    if appFound then
+      hs.application.launchOrFocusByBundleID(appFound:bundleID())
+    else
+      hs.alert("No app found: " .. app)
+    end
+  end
+end
+
+
+local apps = {
+  P=launcher('Slack'),
+  O=launcher('Google Chrome'),
+  I=launcher('iTerm'),
+}
+
+local launch_modifier = {"ctrl", "alt"}
+for char, launcher in pairs(apps) do
+  hotkey.deleteAll(launch_modifier, char)
+  hotkey.bind(launch_modifier, char, launcher)
 end
 
 setGrid()
