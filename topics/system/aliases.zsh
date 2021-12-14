27011b00904b31783a2c174d039bc843e3d9dbd3
Joshua Frederick <josh@jfred.net>
2021-12-13 19:00:49 -0800

    system: codify default timeout of 2hrs

--- topics/system/aliases.zsh
+++ topics/system/aliases.zsh
@@ -3,4 +3,4 @@ alias l="ls -lAh"
 alias ll="ls -l"
 alias la='ls -A'
 
-alias ssh-add-all="find ~/.ssh -type f -name 'id_*' -not -name '*.pub' -execdir ssh-add {} \;"
+alias ssh-add-all="find ~/.ssh -type f -name 'id_*' -not -name '*.pub' -execdir ssh-add -t 2h {} \;"
