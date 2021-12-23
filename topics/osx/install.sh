14bfcb412490a1b35dfdd32a525536785113bfe9
Joshua Frederick <josh@jfred.net>
2021-12-23 09:13:30 -0800

    osx: move to Brewfile to speedup reinstalls

--- topics/osx/install.sh
+++ topics/osx/install.sh
@@ -5,5 +5,16 @@ BASEDIR=$(dirname $0)
 # base system settings
 $BASEDIR/settings.sh
 
-# install base packages
-$BASEDIR/homebrew_install.sh
+# Make sure we’re using the latest Home
+echo Updating homebrew...
+brew update
+
+echo "Installing homebrew formulas and casks..."
+brew bundle install
+
+echo Upgrading existing homebrew formulas...
+brew upgrade
+
+# Remove outdated versions from the cellar
+echo Homebrew cleanup...
+brew cleanup
