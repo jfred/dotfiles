01fd3d07c8d16421fed1dbdc0a5ec02cfdc33c96
Joshua Frederick <josh@jfred.net>
2022-11-13 08:36:41 -0800

    zsh: update config to speedup startup time

--- topics/osx/config.zsh
+++ topics/osx/config.zsh
@@ -1,1 +1,22 @@
 PATH="/usr/local/opt/gnu-units/libexec/gnubin:$PATH"
+
+if [[ "${TERM_PROGRAM}" = "iTerm.app" ]]; then
+    # setup iterm profile
+    declare -A iterm_envs
+    # usage: iterm_add_profile name profilename
+    iterm_add_env() {
+        iterm_envs[${1}]=${2}
+    }
+
+    # default iterm env profiles
+    iterm_add_env prod prod
+    iterm_add_env stg stage
+    iterm_add_env test test
+    iterm_add_env dev test
+
+    # setup iterm specific env_switch
+    env_switch() {
+        profile=${iterm_envs[$1]:-Default}
+        echo -n -e "\033]50;SetProfile=${profile}\a"
+    }
+fi
---
+++ topics/tmux/config.zsh
@@ -0,0 +1,22 @@
+if [[ "${TERM_PROGRAM}" = "tmux" ]]; then
+    # Terminal color profile helpers
+    declare -A tmux_envs_fgmap
+    declare -A tmux_envs_bgmap
+    # usage: tmux_add_profile name fgcolor bgcolor
+    tmux_add_env() {
+        tmux_envs_fgmap[$1]=${2}
+        tmux_envs_bgmap[$1]=${3:-default}
+    }
+
+    # default tmux env profiles
+    tmux_add_env prod default colour52
+    tmux_add_env stg default colour58
+    tmux_add_env test default colour22
+    tmux_add_env dev default colour18
+
+    env_switch() {
+        fgcolour=${tmux_envs_fgmap[$1]:-default}
+        bgcolour=${tmux_envs_bgmap[$1]:-default}
+        tmux select-pane -P "bg=${bgcolour},fg=${fgcolour}"
+    }
+fi
