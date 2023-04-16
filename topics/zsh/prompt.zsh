f0977f58c0b31fba474289d16a6544bad3fb0087
Joshua Frederick <josh@jfred.net>
2023-04-16 11:41:09 -0700

    zsh: fix zsh config to prevent double loading of source files

--- topics/zsh/prompt.zsh
+++
@@ -1,8 +0,0 @@
-setopt promptsubst
-
-# zgitinit and prompt_wunjo_setup must be somewhere in your $fpath, see README for more.
-
-# Load the prompt theme system
-autoload -U promptinit
-promptinit
-prompt jfred
