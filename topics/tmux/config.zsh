e2af4f0123974634140922923f11ca6a753f06f1
Joshua Frederick <josh@jfred.net>
2024-05-10 06:52:38 -0700

    tmux: Add tmux binding to switch env (e)

--- topics/tmux/config.zsh
+++ topics/tmux/config.zsh
@@ -1,22 +1,5 @@
 if [[ "${TERM_PROGRAM}" = "tmux" ]]; then
-    # Terminal color profile helpers
-    declare -g -A tmux_envs_fgmap
-    declare -g -A tmux_envs_bgmap
-    # usage: tmux_add_profile name fgcolor bgcolor
-    tmux_add_env() {
-        tmux_envs_fgmap[$1]=${2}
-        tmux_envs_bgmap[$1]=${3:-default}
-    }
-
-    # default tmux env profiles
-    tmux_add_env prod white colour52
-    tmux_add_env stg color00 colour58
-    tmux_add_env test white colour22
-    tmux_add_env dev white colour18
-
     env_switch() {
-        fgcolour=${tmux_envs_fgmap[$1]:-default}
-        bgcolour=${tmux_envs_bgmap[$1]:-default}
-        tmux select-pane -P "bg=${bgcolour},fg=${fgcolour}"
+        tmux.env_switch $1
     }
 fi
