7be9a396fe821de4cc39799ee1cae78e5eceffe7
Joshua Frederick <josh@jfred.net>
2024-07-31 07:04:18 -0700

    system: swap alias for function for ssh-add-all

--- topics/system/aliases.zsh
+++ topics/system/aliases.zsh
@@ -2,5 +2,3 @@ alias ls="ls -F --color=auto"
 alias l="ls -lAh"
 alias ll="ls -l"
 alias la='ls -A'
-
-alias ssh-add-all="find ~/.ssh -type f -name 'id_*' -not -name '*.pub' -execdir ssh-add -t 6h {} \;"
--- topics/system/config.zsh
+++ topics/system/config.zsh
@@ -11,3 +11,8 @@ if type direnv > /dev/null; then
     # reload direnv incase launched in directory sub .envrc
     direnv reload 2> /dev/null
 fi
+
+
+function ssh-add-all {
+    find ${1:-~/.ssh} -type f -name 'id_*' -not -name '*.pub' -execdir ssh-add -t 6h {} \;
+}
