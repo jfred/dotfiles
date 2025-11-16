e7a5e332680f52254eaf3d9fc665f9951c062d4c
Joshua Frederick <josh@jfred.net>
2025-11-15 18:52:52 -0800

    nvim: replace vim-airline with lualine.nvim for improved statusline configuration

--- topics/nvim/config/lua/plugins/ui.lua
+++ topics/nvim/config/lua/plugins/ui.lua
@@ -2,11 +2,24 @@
 return {
   -- Statusline
   {
-    "vim-airline/vim-airline",
-    dependencies = { "vim-airline/vim-airline-themes" },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      vim.g.airline_powerline_fonts = 0
-      vim.opt.laststatus = 2
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+          component_separators = { left = "|", right = "|" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
     end,
   },
 
