c7c09a342f99736f82253ca45eb071ec67463f30
Joshua Frederick <josh@jfred.net>
2025-11-15 20:39:47 -0800

    nvim: add gitsigns.nvim for Git integration and hunk management

--- topics/nvim/config/lua/plugins/coding.lua
+++ topics/nvim/config/lua/plugins/coding.lua
@@ -26,6 +26,43 @@ return {
 
   -- Git
   { "tpope/vim-fugitive" },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "+" },
+          change = { text = "~" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+          local opts = { buffer = bufnr }
+
+          -- Navigation
+          vim.keymap.set("n", "]c", function()
+            if vim.wo.diff then return "]c" end
+            vim.schedule(function() gs.next_hunk() end)
+            return "<Ignore>"
+          end, vim.tbl_extend("force", opts, { expr = true, desc = "Next hunk" }))
+
+          vim.keymap.set("n", "[c", function()
+            if vim.wo.diff then return "[c" end
+            vim.schedule(function() gs.prev_hunk() end)
+            return "<Ignore>"
+          end, vim.tbl_extend("force", opts, { expr = true, desc = "Prev hunk" }))
+
+          -- Actions
+          vim.keymap.set("n", "<Leader>hs", gs.stage_hunk, vim.tbl_extend("force", opts, { desc = "Stage hunk" }))
+          vim.keymap.set("n", "<Leader>hr", gs.reset_hunk, vim.tbl_extend("force", opts, { desc = "Reset hunk" }))
+          vim.keymap.set("n", "<Leader>hp", gs.preview_hunk, vim.tbl_extend("force", opts, { desc = "Preview hunk" }))
+          vim.keymap.set("n", "<Leader>hb", function() gs.blame_line({ full = true }) end, vim.tbl_extend("force", opts, { desc = "Blame line" }))
+        end,
+      })
+    end,
+  },
 
   -- Testing
   {
